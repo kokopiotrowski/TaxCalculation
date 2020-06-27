891497d9facb95699e8a598e3043ba0ead8cae1d
Konrad Piotrowski <konrad.piotrowski00@gmail.com>
2020-06-27 20:07:56 +0200

    catching exceptions when entering numbers

--- TaxCalculation/Program.cs
+++ TaxCalculation/Program.cs
@@ -6,19 +6,38 @@ namespace TaxCalculation
     {
         static void Main(string[] args)
         {
+            int x = 0;
             while (true)
             {
                 Console.Write("Write:\n 1 - calculate your tax\n 2 - exit the program\n");
-
-                int x = int.Parse(Console.ReadLine());
+                try
+                {
+                    x = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    x = -1;   
+                }
 
                 switch(x)
                 {
                     case 1:
-                        Console.WriteLine("Enter your yearly earnings (currency PLN)");
-                        double earnings = double.Parse(Console.ReadLine());
-                        Console.WriteLine("Enter ammount of kids you have: ");
-                        int kids = int.Parse(Console.ReadLine());
+
+                        double earnings;
+                        int kids;
+                        try
+                        {
+                            Console.WriteLine("Enter your yearly earnings (currency PLN)");
+                            earnings = double.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter ammount of kids you have: ");
+                            kids = int.Parse(Console.ReadLine());
+                        }
+                        catch(Exception)
+                        {
+                            Console.WriteLine("Input only numbers. ");
+                            break;
+                        }
+                        
                         Console.WriteLine("Tax you need to pay: " + TaxCalculator.GetTaxForEarnings(earnings)
                             + "\nBonus for children: " + TaxCalculator.GetBonusForChildren(kids));
                         break;
