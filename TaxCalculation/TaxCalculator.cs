b1b336fe5ea551221aee9b88607fe2455c7573d5
Konrad Piotrowski <konrad.piotrowski00@gmail.com>
2020-06-27 19:58:53 +0200

    TaxCalculator class

---
+++ TaxCalculation/TaxCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxCalculation
+{
+    static class TaxCalculator
+    {
+        public static double GetTaxForEarnings(double earnings)
+        {
+            if(earnings <8000)
+            {
+                return 0;
+            }
+            else if(earnings <= 85000)
+            {
+                return earnings * 0.18;
+            }
+            else
+            {
+                return earnings * 0.21;
+            }
+        }
+
+        public static double GetBonusForChildren(int amountOfChildren)
+        {
+            return 500 * amountOfChildren;
+        }
+
+    }
+}
